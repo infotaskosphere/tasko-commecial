7c9f58c03b9264cf98f3239a5c9f15aa0e58ce36
infotaskosphere <info.taskosphere@gmail.com>
2026-09-25 12:12:18 +0530

    backend: remove unused legacy health_monitor.py wrapper

--- backend/health_monitor.py
+++
@@ -1,7 +0,0 @@
-# Taskosphere Licensing Modules
-from backend.licensing.license_engine import LicenseEngine
-from backend.licensing.activation_engine import ActivationEngine
-from backend.licensing.usage_tracker import UsageTracker
-from backend.licensing.feature_unlock import FeatureUnlock
-from backend.licensing.customer_license import CustomerLicense
-from backend.licensing.enterprise_license import EnterpriseLicense
