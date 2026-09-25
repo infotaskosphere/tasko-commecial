13030b718d4467e3e63167c563b4bc9e42c1bd62
infotaskosphere <info.taskosphere@gmail.com>
2026-09-25 12:12:21 +0530

    backend: remove unused legacy secret_manager.py wrapper

--- backend/secret_manager.py
+++
@@ -1,6 +0,0 @@
-# Taskosphere Enterprise Search Modules
-from backend.search.enterprise_search import EnterpriseSearch
-from backend.search.document_search import DocumentSearch
-from backend.search.ledger_search import LedgerSearch
-from backend.search.semantic_search import SemanticSearch
-from backend.search.vector_search import VectorSearch
